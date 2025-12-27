aaeed6396a1e7cd5a979a62809ff2daab1a44ad2
Remake1 <deerteeu@gmail.com>
2025-12-27 14:51:37 -0600

    init

---
+++ src/entrypoints/popup/App.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div>
+    <a href="https://wxt.dev" target="_blank">
+      <img src="/wxt.svg" class="logo" alt="WXT logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+    Hello world
+  </div>
+</template>
+
+<style scoped>
+
+</style>
